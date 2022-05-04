ccc534103e0661aa4e3f3c597c45605af7daa617
Oktovivian <68942070+Oktovivian@users.noreply.github.com>
2022-05-04 21:18:40 +0700

    Add files via upload

---
+++ (002) kelas dan instance.py
@@ -0,0 +1,16 @@
+# kelas mahasiswa sebagai "definisi"
+class Mahasiswa:
+    nama = None
+    nim = None
+    alamat = None
+    telepon = None
+    email = None
+
+Mahasiswa1 = Mahasiswa()
+Mahasiswa1.nama = "Rizki"
+Mahasiswa1.nim = "18081010012"
+Mahasiswa1.alamat = "Jl.Kebon Jeruk"
+Mahasiswa1.telepon = "0812121212"
+Mahasiswa1.email = "rizkyganteng@gmail.com"
+
+print(Mahasiswa1.nama," ", Mahasiswa1.nim," ", Mahasiswa1.alamat," ", Mahasiswa1.telepon," ", Mahasiswa1.email)
