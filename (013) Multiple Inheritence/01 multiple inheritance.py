a3317626422ae26dcbdb006f826f3545e69e19e5
Oktovivian <68942070+Oktovivian@users.noreply.github.com>
2022-05-12 22:29:08 +0700

    Delete (013) Multiple Inheritence directory

--- (013) Multiple Inheritence/01 multiple inheritance.py
+++
@@ -1,8 +0,0 @@
-class Induk1:
-  pass
-
-class Induk2:
-  pass
-
-class Turunan(Induk1, Induk2):
-  pass
--- (013) Multiple Inheritence/02 multi level inheritance.py
+++
@@ -1,8 +0,0 @@
-class Induk:
-  pass
-
-class Turunan1(Induk):
-  pass
-
-class Turunan2(Turunan1):
-  pass
