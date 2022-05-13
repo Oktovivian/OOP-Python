ae19e49a2b8c1e0de26024c5e9a8e3cc5e36b95a
Oktovivian <68942070+Oktovivian@users.noreply.github.com>
2022-05-13 22:07:44 +0700

    Add files via upload

---
+++ (014) Static Variable/01 instance variable.py
@@ -0,0 +1,9 @@
+class Universitas:
+    def __init__(self, nama):
+        self.nama = nama
+
+Diponegoro = Universitas('Diponegoro')
+Brawijaya = Universitas('Brawijaya')
+
+print(Diponegoro.nama)
+print(Brawijaya.nama)
